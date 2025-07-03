47adb50a2744da8cbaf0ef72e4b7e637a8c6502d
Hovhannes Sahakyan <hovhannes@superannotate.com>
2025-07-04 00:43:48 +0200

    fix: add credentials to CORS policy

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
       origin: (_, cb) => cb(null, AccessControlAllowOrigins),
+      credentials: true,
     },
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
