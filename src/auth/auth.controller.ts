d81074434ed05f84f56931a79b66d7047925c97a
Hovhannes Sahakyan <hovhannes@superannotate.com>
2025-07-04 02:35:41 +0200

    fix: cookie not deleting on logout

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,7 +4,7 @@ import { SignInDto } from './dto/sign-in.dto';
 import { SignUpDto } from './dto/sign-up.dto';
 import { UserService } from 'src/user/user.service';
 import { Response } from 'express';
-import { ACCESS_TOKEN } from 'src/constants';
+import { ACCESS_TOKEN, COOKIE_OPTIONS } from 'src/constants';
 import { tomorrow } from 'src/utils';
 
 @Controller('api/v1/auth')
@@ -20,17 +20,14 @@ export class AuthController {
         const { user, token } = await this._authService.signIn(email, password);
         response.cookie(ACCESS_TOKEN, token, {
             expires: tomorrow(),
-            sameSite: 'none',
-            secure: true,
-            httpOnly: true,
+            ...COOKIE_OPTIONS,
         });
         return response.json(user);
     }
 
     @Post('logout')
     logout(@Res() response: Response) {
-        response.clearCookie(ACCESS_TOKEN);
-        return response.status(HttpStatus.OK).send();
+        return response.clearCookie(ACCESS_TOKEN, COOKIE_OPTIONS).status(HttpStatus.OK).send();
     }
 
     @Post('register')
--- src/constants.ts
+++ src/constants.ts
@@ -6,3 +6,9 @@ export const enum ROLES {
     ADMIN = 1,
     REGULAR = 2,
 }
+
+export const COOKIE_OPTIONS = {
+    sameSite: 'none',
+    secure: true,
+    httpOnly: true,
+} as const;
